60ccf4b7e37e0d32c702153848659f4d3886424a
Mayur V Jangale <25575266+mayur-jangale@users.noreply.github.com>
2023-01-12 13:19:40 +0530

    Delete votes directory

--- votes/application/views/footer.php
+++
@@ -1,4 +0,0 @@
-<a href="https://www.classbroom.me">Home</a> | <a href="https://social.classbroom.me">Globe</a> | <a href="https://chat.classbroom.me">English Practice</a> | <a href="https://test.classbroom.me">Tests</a> | 
-<a href="https://forum.classbroom.me">Questions</a> | <a href="https://notes.classbroom.me">Notes</a> | <a href="https://dictionary.classbroom.me">Dictionary</a> 
-<hr>
-        <font style="color:black">   &copy; Classbroom.me | All Rights Reserved</font>
